215aa177ad290eac46c0a50fb823a5a04beb0aef
AVCADO <russon@awty.org>
2020-02-05 14:02:00 -0600

    additions

---
+++ bootloader/boot3.s
@@ -0,0 +1,44 @@
+.code16 #generate 16-bit code
+.text #executable code location
+    .globl _start;
+_start: #code entry point
+
+    /* first word */
+    #print letter 'P'
+    movb $'P', %al
+    movvb $0x0e, %ah
+    int $0x10
+
+    #print letter 'l'
+    movb 'l', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    #print letter 'e'
+    movb 'e', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    #print letter 'a'
+    movb 'a', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    #print letter 's'
+    movb 's', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    #print letter 'e'
+    movb 'e', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    #print letter ' '
+    movb ' ', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    . = _start + 510 #mov to 510th byte from 0 pos
+    .byte 0x55 #append boot signature   
+    .byte 0xaa #append boot signature
