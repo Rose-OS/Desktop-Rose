553f0c4a4ab1677b6b6f11e3167fdf94975f1fb0
AVCADO <russon@awty.org>
2020-02-04 20:16:27 -0600

    Initial commit

---
+++ bootloader/boot.s
@@ -0,0 +1,7 @@
+.code16                   #generate 16-bit code
+.text                     #executable code location
+     .globl _start;
+_start:                   #code entry point
+     . = _start + 510     #mov to 510th byte from 0 pos
+     .byte 0x55           #append boot signature
+     .byte 0xaa           #append boot signature
