5b6904c2d1e41d215a6925f1dad29268bbfd9ae9
AVCADO <russon@awty.org>
2020-02-05 20:46:04 -0600

    new

--- bootloader/boot3.s
+++ bootloader/boot3.s
@@ -41,8 +41,36 @@ _start: #code entry point
     int $0x10
 
     #print letter 'n'
-    movb 'r', 
+    movb 'r', %al
+    movb $0x0e, %ah
+    int $0x10
 
+    #print letter 'y'
+    movb 'y', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    #print letter ' ' (space)
+    movb ' ', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    /* third word */
+    #print letter 'k'
+    movb 'k', %al
+    movb $0x0e, %ah
+    int $0x10
+    
+    #print letter 'e'
+    movb 'e', %al
+    movb $0x0e, %ah
+    int $0x10
+
+    #print letter 'y'
+    movb 'y', %al
+    movb $0x0e, %ah
+    int $0x10
+    
     . = _start + 510 #mov to 510th byte from 0 pos
     .byte 0x55 #append boot signature   
     .byte 0xaa #append boot signature
