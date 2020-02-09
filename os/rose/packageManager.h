1d0dcafcbe987d8ec393d4966446036943290805
AVCADO <russon@awty.org>
2020-02-09 08:44:49 -0600

    Auto-Added by script

---
+++ os/rose/packageManager.h
@@ -0,0 +1,21 @@
+// Normal imports
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+// This directory imports
+#include "getPackages.h"
+
+// Typedef's / Structs
+typedef struct Packages {
+    float bSize; // we all know a package cannot be <0 bytes
+    float mbSize; // If mbSize = 0, then
+    // do nothing
+    float kbSize; // If kbSize = 0, then
+    // do nothing
+    float gbSize; // If gbSize = 0, then
+    // do nothing
+} Packages;
+
+// Functions
---
+++ os/rose/rose.c
@@ -0,0 +1,18 @@
+// Normal imports
+#include <stdio.h>
+#include <stdlib.h>
+
+// Linux imports
+
+
+// Actual (this dir) imports
+#include "packageManager.h"
+#include "getPackages.h"
+
+// External functions (calling, etc)
+
+
+// Main function
+int main(){
+    printf("Hi RoseOS");
+}
