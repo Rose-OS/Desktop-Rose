0df5d7089d0b765fb0366234879bdef24f76a0a0
AVCADO <russon@awty.org>
2020-02-10 10:52:09 -0600

    Extra assembly file; finished bootloader

---
+++ bootloader/filesMissing.asm
@@ -0,0 +1,34 @@
+BITS 16
+
+start:
+mov ax, 07C0h
+add ax, 288
+mov ss, ax
+mov sp, 4096
+
+mov ax, 07C0h
+mov ds, ax
+
+mov si, msg
+
+call print_string
+
+jmp $
+
+msg db 'Important files are missing... Please reboot', 0
+
+print_string:
+mov ah, 0Eh
+
+.repeat:
+lodsb
+cmp al, 0
+je .done
+int 10h
+jmp .repeat
+
+.done:
+ret
+
+times 510-($-$$) db 0
+dw 0xAA55
