b2691126ccda22c4970ca4271789e413b1354e6a
AVCADO <russon@awty.org>
2020-02-11 13:05:10 -0600

    C

---
+++ bootloader/build/setup/setup.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+// Placeholder code
+int main() { return 0; }
---
+++ bootloader/build/setup/setup1.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ret return
+#define null NULL
+
+
+typedef struct Criteria { 
+    char name[90]; // Name of file
+    float size; // Size (in mb)
+} Criteria;
+
+
+
+void example(Criteria criteria1){
+    Criteria criteria1; // create instance of criteria
+    
+    // all functions must have this
+    // below line 16
+
+    // Error handling
+    if (criteria1.name == " " || criteria1.name == ""){
+        printf("Illegal size");
+    } else { ret; }
+    if (criteria1.name == null){
+        ret;
+    } else { }
+}
